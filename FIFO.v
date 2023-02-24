5f8525c17cbf41139a24730f7fa91daa401d8404
srburne2 <srburne2@ncsu.edu>
2023-02-24 18:55:29 -0500

    first commit

---
+++ FIFO.v
@@ -0,0 +1,111 @@
+module cdc_fifo #(parameter DATA_WIDTH = 8) (
+    input wire clock_write,
+	input wire clock_read,
+    input wire reset,
+    input wire [DATA_WIDTH-1:0] write_data,
+    input wire write_enable,
+	input wire read_next,
+    output wire read_empty,
+    output wire write_full,
+    output reg [DATA_WIDTH-1:0] read_data
+);
+
+	// Parameters
+	parameter ADDR_WIDTH = 4;   // Address width of the FIFO
+
+	// The number of addresses in the stack
+	localparam STACK_NUM_ADDY = 16;//2 ** ADDR_WIDTH; TODO revert back
+
+	// Local variables. Read Pointer and Sync Registers
+	reg [ADDR_WIDTH-1:0] read_ptr_next;
+	reg [ADDR_WIDTH-1:0] read_ptr_next_pipe;
+	reg [ADDR_WIDTH-1:0] read_ptr_next_write_side;
+	
+	// Local variables. Write Pointer and Sync Registers
+	reg [ADDR_WIDTH-1:0] write_ptr_next;
+	reg [ADDR_WIDTH-1:0] write_ptr_next_pipe;
+	reg [ADDR_WIDTH-1:0] write_ptr_next_read_side;
+	
+	// 2D array
+	reg [DATA_WIDTH-1:0] fifo_data [STACK_NUM_ADDY-1:0];
+	
+	// The FIFO is empty (from the read side) when the read_pointer_next equals the write_ptr_next
+	assign read_empty = (read_ptr_next == write_ptr_next_read_side);
+	
+	// The FIFO is full (from the write side) when the write_ptr_next + 1 equals the read_ptr_next_write_side
+	// This represents right before we are about to loop back around
+	// Note the natural loop-around overflow through chopping off the bits
+	// TODO: potentially chop the bits here
+	wire [ADDR_WIDTH-1:0] write_ptr_next_plus1, read_ptr_next_plus1;
+	assign write_ptr_next_plus1 = write_ptr_next + 1;
+	assign read_ptr_next_plus1 = read_ptr_next + 1;
+
+	assign write_full = (write_ptr_next_plus1 == read_ptr_next_write_side);
+	
+	// Just here for looping
+	integer i;
+
+	// Handle what happens on the edge of the write clock
+	always @(posedge clock_write) begin
+		// Handle the reset case
+		if (reset == 1) begin
+			// Reset the clock domain sync registers
+			read_ptr_next_pipe <= 0;
+			read_ptr_next_write_side <= 0;
+			// Reset the fifo data registers
+			
+			for (i=0; i<=STACK_NUM_ADDY; i = i + 1) begin
+				fifo_data[i] <= 0;
+			end
+			// Reset the write pointer
+			write_ptr_next <= 0;
+		end
+		
+		// Handle the typical case (no reset)
+		else begin
+			// Get the read pointer over to the write clock domain
+			read_ptr_next_pipe <= read_ptr_next;
+			read_ptr_next_write_side <= read_ptr_next_pipe;
+			
+			// If the fifo isn't full and the write is enabled
+			// Load the new data and increment the write pointer
+			if ((write_full == 0) && (write_enable == 1)) begin
+				fifo_data[write_ptr_next] <= write_data;
+				write_ptr_next <= write_ptr_next_plus1;//[ADDR_WIDTH:0]; TODO add this back?
+			end
+		end
+	end
+	
+	// Handle what happens on the edge of the read clock
+	always @(posedge clock_read) begin
+		// Handle the reset case
+		if (reset == 1) begin
+			// Reset the clock domain sync registers
+			write_ptr_next_pipe <= 0;
+			write_ptr_next_read_side <= 0;
+			// Reset the read pointer
+			read_ptr_next <= 0;
+		end
+		
+		// Handle the typical case (no reset)
+		else begin
+			// Get the write pointer over to the read clock domain
+			write_ptr_next_pipe <= write_ptr_next;
+			write_ptr_next_read_side <= write_ptr_next_pipe;
+			
+			// If the fifo isn't empty and the read is enabled
+			// increment the read pointer
+			if ((read_empty == 0) && (read_next == 1)) begin
+				read_ptr_next <= read_ptr_next_plus1;//[ADDR_WIDTH-1:0]; TODO add this back?
+			end
+		end
+	end
+	
+	// Output the read data
+	reg [ADDR_WIDTH-1:0] temp;
+	always @(*) begin
+		temp = read_ptr_next;//-1
+		read_data = fifo_data[temp];//[ADDR_WIDTH-1:0]; TODO add this back?
+	end
+	
+endmodule
---
+++ TB_FIFO.sv
@@ -0,0 +1,117 @@
+`timescale 1ns/1ns
+
+module cdc_fifo_tb;
+	
+	parameter DATA_WIDTH = 8;
+
+	// Inputs
+	reg clock_write;
+	reg clock_read;
+    reg reset;
+    reg [DATA_WIDTH-1:0] write_data;
+    reg write_enable;
+	reg read_next;
+
+	// Outputs
+    wire read_empty;
+    wire write_full;
+    wire [DATA_WIDTH-1:0] read_data;
+
+	// Good reference https://www.realdigital.org/doc/43c79714a7f3d0bbb8098d60c63fde48
+	cdc_fifo #( .DATA_WIDTH(DATA_WIDTH) ) dut (
+		.clock_write(clock_write),
+		.clock_read(clock_read),
+		.reset(reset),
+		.write_data(write_data),
+		.write_enable(write_enable),
+		.read_next(read_next),
+		.read_empty(read_empty),
+		.write_full(write_full),
+		.read_data(read_data)
+	);
+	
+	// Note: we can access and assert signals within the dut like this
+	// assert(dut.registerA === 2'b01);
+
+	// Write clock generation
+	initial begin
+		clock_write = 1;
+		while (1) begin
+			#5;
+			clock_write = ~clock_write;
+			// Rising edge occurs @ multiples of 10
+		end
+	end
+	
+	// Read clock generation
+	initial begin
+		clock_read = 1;
+		while (1) begin
+			#3;
+			clock_read = ~clock_read;
+			// Rising edge occurs @ multiples of 6
+		end
+	end
+
+	// Test write, read, and reset
+	initial begin
+		
+		/* Key Awareness
+		 * The first read will always get a value of zero
+		 * 
+		 */
+	
+		// Assert reset
+		reset			= 1;
+		write_data		= 0;
+		write_enable	= 0;
+		read_next		= 0;
+		#11; // change inputs and assert outputs shortly after the clock
+		assert(read_empty	=== 1);
+		assert(write_full	=== 0);
+		assert(read_data	=== 0);
+		
+		// Assert that the stack fills up
+		reset 			= 0;
+		write_enable	= 1;
+		
+		for (int i = 1; i <= 14; i = i + 1) begin
+			write_data = i;	//left bits chop off by default
+			#10;
+		end
+		
+		// Assert that the stack is not full after 14 writes and 0 reads
+		assert(read_empty	=== 0);
+		assert(write_full	=== 0);
+		assert(read_data	=== 1);
+		
+		write_data = 15;
+		#10;
+		
+		// Assert that the stack is full after 15 writes and 0 reads
+		assert(read_empty	=== 0);
+		assert(write_full	=== 1);
+		assert(read_data	=== 1);
+		
+		// See if attempting to write this will corrupt the program
+		write_data = 16;
+		#10;
+		
+		// Assert that the stack is still full after 15 writes, a false write, and 0 reads
+		assert(read_empty	=== 0);
+		assert(write_full	=== 1);
+		assert(read_data	=== 1);
+		
+		
+		// Now start reading data back from the FIFO and assert that the data is correct
+		write_enable	= 0;
+		read_next		= 1;
+		for (int i = 2; i <= 15; i = i + 1) begin
+			#6;
+			assert(read_data === i);
+		end
+		
+		// Let's see how this is working so far		
+		$finish;
+	end
+endmodule
